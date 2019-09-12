ffdbf083f7d5e5b714bbf3bdde0cf0594eb7f3f3
halil <halil4deen@gmail.com>
2019-09-12 20:52:15 +0100

    added procfile, added domains to allowed_hosts in settings file, reconfigured my db to sqlite3

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -28,7 +28,10 @@ SECRET_KEY = '-pe1g=o2gcu5mmp7m@++(t$xo7x91wr8k@p9ii*2fs_43^@2b='
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'halilblogproject.herokuapp.com',
+    '127.0.0.1'
+]
 
 
 # Application definition
@@ -81,12 +84,8 @@ WSGI_APPLICATION = 'blogproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('DB_NAME'),
-        'USER': config('DB_USER'),
-        'PASSWORD': config('DB_PASS'),
-        'HOST': config('DB_HOST'),
-        'PORT': config('DB_PORT'),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
